d0afd003380753871d8aec9dff3646ad3312db98
aliensbright <144503232+aliensbright@users.noreply.github.com>
2024-10-11 11:14:33 -0700

    still not working

--- endpoint.py
+++ endpoint.py
@@ -1,5 +1,39 @@
 from flask import Flask, request
 from flask_cors import CORS
 
-app = Flask(___name___)
+app = Flask(__name__)
 CORS(app)
+
+@app.route("/form.html", methods=['POST'])
+def endpoint():
+    data = request.form
+    print(data)
+    output = f"""
+    <!doctype html>
+    <head>
+        <title> Form Result </title>
+        <style>
+            .result {{
+                color: blue;
+                font-size: 2em;
+            }}
+        </style>
+    </head>
+    <body>
+        <div class='result'>Today we went to {data('pname')} Lake. At {data('pname')} 
+        Lake, we saw a {data('animal')}. Since I am not scared of {data('animal')}s, I 
+        went to pet it with my hand. The {data('animal')} was very {data('adjective')} when I 
+        pet it. My friend, {data('fname')}, on the other hand, felt very {data('emotion')}
+        and poked it with their {data('tool')} weapon. The {data('animal')} ended up 
+        {data('verb')} us. The end.
+        </div>
+    </body>
+    """
+    return output 
+
+@app.route("/", methods=["POST"])
+def anotherFunction():
+    pass
+
+
+app.run(port=5000)
