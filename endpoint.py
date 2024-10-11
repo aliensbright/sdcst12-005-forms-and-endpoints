b14c0b09ddebf59c9a3a6af5265cacf1efc030a6
aliensbright <144503232+aliensbright@users.noreply.github.com>
2024-10-11 11:34:45 -0700

    Just need to format stuff

--- endpoint.py
+++ endpoint.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/form.html", methods=['POST'])
+@app.route("/question", methods=['POST'])
 def endpoint():
     data = request.form
     print(data)
@@ -14,21 +14,23 @@ def endpoint():
         <title> Form Result </title>
         <style>
             .result {{
-                color: blue;
-                font-size: 2em;
+                color: black;
+                font-size: 1.5em;
             }}
         </style>
     </head>
     <body>
-        <div class='result'>Today we went to {data('pname')} Lake. At {data('pname')} 
-        Lake, we saw a {data('animal')}. Since I am not scared of {data('animal')}s, I 
-        went to pet it with my hand. The {data('animal')} was very {data('adjective')} when I 
-        pet it. My friend, {data('fname')}, on the other hand, felt very {data('emotion')}
-        and poked it with their {data('tool')} weapon. The {data('animal')} ended up 
-        {data('verb')} us. The end.
+        <h1 style="font-size:2em">Day at the Lake</h1>
+        <div class='result'>Today we went to {data['pname']} Lake. At {data['pname']} 
+        Lake, we saw a {data['animal']}. Since I am not scared of {data['animal']}s, I 
+        went to pet it with my hand. The {data['animal']} was very {data['adjective']} when I 
+        pet it. My friend, {data['fname']}, on the other hand, felt very {data['emotion']}
+        and poked it with their {data['tool']} weapon. The {data['animal']} ended up 
+        {data['verb']} us. The end.
         </div>
     </body>
     """
+    print(output)
     return output 
 
 @app.route("/", methods=["POST"])
